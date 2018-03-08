230ec1819d81cff0db8f3856947cacf2f188377a
atiq <joseph1atiq@gmail.com>
2018-03-08 16:44:45 +0100

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  contact = {
+    name : "jose",
+
+    email: "jose@gmail.com",
+
+  }
 }
